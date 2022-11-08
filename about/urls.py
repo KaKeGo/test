4e45ee68c0056429f1a18139c6ead65cf5f85cb7
KaKeGo <funfcia@hotmail.com>
2022-11-08 20:25:56 +0100

    create about serializer list add path and views

--- about/urls.py
+++ about/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-# from .views import (
-
-# )
+from .views import (
+    about_list_view,
+)
 
 app_name = 'about'
 
 urlpatterns = [
-
+    path('', about_list_view, name='about'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+from .models import About
+from .serializer import (
+    AboutSerializer
+)
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def about_list_view(request):
+    about = About.objects.all()
+    if not about.exists:
+        return Response({'message': 'Something went wrong'})
+    serializer = AboutSerializer(about, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
