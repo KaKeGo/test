593739e4da74e5a69ec617d3531ccf1f7903dc69
KaKeGo <funfcia@hotmail.com>
2022-11-08 19:52:29 +0100

    Create model for about app and add model to admin

--- about/models.py
+++ about/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class About(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    img = models.ImageField(upload_to='about/')
+    
+    class Meta:
+        ordering = ['-id']
+        
+    def __str__(self):
+        return f'{self.title}'
